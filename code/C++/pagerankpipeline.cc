0b647c701244ab1d97da1a6b7ed445961614e85a
Bradley C. Kuszmaul <bradley@mit.edu>
2015-10-24 00:54:18 -0400

    Write sorted data

--- code/C++/pagerankpipeline.cc
+++ code/C++/pagerankpipeline.cc
@@ -12,13 +12,13 @@
 
 #include <sys/stat.h>
 
-static std::string dir_named(int SCALE)  {
-  return "data/K" + std::to_string(SCALE);
+static std::string dir_named(int kernel, int SCALE)  {
+  return "data/K" + std::to_string(kernel) + "_S" + std::to_string(SCALE);
 }
   
 
-static std::string file_named(int SCALE, int filenum) {
-  return dir_named(SCALE) + "/" + std::to_string(filenum) + ".tsv";
+static std::string file_named(int kernel, int SCALE, int filenum) {
+  return dir_named(kernel, SCALE) + "/" + std::to_string(filenum) + ".tsv";
 }
 
 
@@ -27,22 +27,22 @@ void kernel0(int SCALE, int edges_per_vertex, int n_files) {
   fasttime_t start = gettime();
   mkdir("data", 0777);
   for (int i = 0; i < n_files; i++) {
-    mkdir(dir_named(SCALE).c_str(), 0777);
+    mkdir(dir_named(0, SCALE).c_str(), 0777);
     if (0) {
-      std::ofstream f(file_named(SCALE, i), std::ios::out);
+      std::ofstream f(file_named(0, SCALE, i), std::ios::out);
       for (const auto &pair : kronecker<T>(SCALE, edges_per_vertex, false)) {
         f << std::get<0>(pair) << '\t' << std::get<1>(pair) << '\n';
       }
       // The stream closes itself when leaving scope.
     } else {
-      write_kronecker(file_named(SCALE, i), SCALE, edges_per_vertex);
+      write_kronecker(file_named(0, SCALE, i), SCALE, edges_per_vertex);
     }
   }
   fasttime_t end   = gettime();
   uint64_t bytes_written = 0;
   for (int i = 0; i < n_files; i++) {
     struct stat statbuf;
-    int r = stat(file_named(SCALE, i).c_str(), &statbuf);
+    int r = stat(file_named(0, SCALE, i).c_str(), &statbuf);
     assert(r == 0);
     bytes_written += statbuf.st_size;
   }
@@ -73,10 +73,11 @@ void kernel1(int SCALE, int edges_per_vertex, int n_files) {
   fasttime_t start = gettime();
   // Sort the data
   std::vector<std::tuple<T, T>> edges;
-  uint64_t M = (1u<<SCALE) * edges_per_vertex * n_files;
+  const uint64_t M_per_file = (1u<<SCALE) * edges_per_vertex;
+  const uint64_t M = M_per_file * n_files;
   edges.reserve(M);
   for (int i = 0; i < n_files; i++) {
-    FILE *f = fopen(file_named(SCALE, i).c_str(), "r");
+    FILE *f = fopen(file_named(0, SCALE, i).c_str(), "r");
     assert(f);
     char *line = NULL;
     size_t len = 0;
@@ -96,6 +97,16 @@ void kernel1(int SCALE, int edges_per_vertex, int n_files) {
   }
   assert(edges.size() == M);
   std::sort(edges.begin(), edges.end());
+  auto it = edges.begin();
+  mkdir(dir_named(1, SCALE).c_str(), 0777);
+  for (int i = 0; i < n_files; i++) {
+    std::ofstream f(file_named(1, SCALE, i), std::ios::out);
+    for (T j = 0; j < M_per_file; j++) {
+      f << std::get<0>(*it) << '\t' << std::get<1>(*it) << '\n';
+      it++;
+    }
+  }
+  assert(it == edges.end());
   fasttime_t end   = gettime();
   printf("scale=%2d Edgefactor=%2d K1time: %9.3fs Medges/sec: %7.2f\n", 
          SCALE, edges_per_vertex, 
