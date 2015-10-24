393294fa3821ae9c968684da708bdf4d24dc2d5b
Bradley C. Kuszmaul <bradley@mit.edu>
2015-10-24 00:39:47 -0400

    sort (but don't write yet)

--- code/C++/pagerankpipeline.cc
+++ code/C++/pagerankpipeline.cc
@@ -6,6 +6,7 @@
 #include "fasttime.h"
 #include "krongraph500.hh"
 
+#include <algorithm>
 #include <iostream>
 #include <fstream>
 
@@ -20,8 +21,9 @@ static std::string file_named(int SCALE, int filenum) {
   return dir_named(SCALE) + "/" + std::to_string(filenum) + ".tsv";
 }
 
+
 template <class T>
-void pagerankpipeline(int SCALE, int edges_per_vertex, int n_files) {
+void kernel0(int SCALE, int edges_per_vertex, int n_files) {
   fasttime_t start = gettime();
   mkdir("data", 0777);
   for (int i = 0; i < n_files; i++) {
@@ -44,12 +46,66 @@ void pagerankpipeline(int SCALE, int edges_per_vertex, int n_files) {
     assert(r == 0);
     bytes_written += statbuf.st_size;
   }
-  printf("Scale=%d Edgefactor=%d K0time: %9.3fs, Medges/sec: %6.2f  bytes:%12ld Mbytes/sec: %6.2f\n", 
+  printf("Scale=%2d Edgefactor=%2d K0time: %9.3fs Medges/sec: %7.2f  bytes:%12ld Mbytes/sec: %6.2f\n", 
          SCALE, edges_per_vertex,
          end-start,
          1e-6 * ((1u<<SCALE)*edges_per_vertex*n_files) / (end-start),
          bytes_written,
          1e-6 * bytes_written / (end-start));
+}
+
+template <class T>
+T parse_int(char *str, char **end) {
+  uint64_t r = 0;
+  while (1) {
+    char c = *str;
+    if (c >= '0' && c <= '9') {
+      r = r*10 + c - '0';
+      str++;
+    } else {
+      *end = str;
+      return r;
+    }
+  }
+}
+template <class T>
+void kernel1(int SCALE, int edges_per_vertex, int n_files) {
+  fasttime_t start = gettime();
+  // Sort the data
+  std::vector<std::tuple<T, T>> edges;
+  uint64_t M = (1u<<SCALE) * edges_per_vertex * n_files;
+  edges.reserve(M);
+  for (int i = 0; i < n_files; i++) {
+    FILE *f = fopen(file_named(SCALE, i).c_str(), "r");
+    assert(f);
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t n_read;
+    while ((n_read = getline(&line, &len, f)) != -1) {
+      char *tab;
+      T v1 = parse_int<T>(line, &tab);
+      assert(tab[0] == '\t');
+      char *nl;
+      T v2 = parse_int<T>(tab+1, &nl);
+      assert(nl[0] == '\n');
+      assert(nl[1] == 0);
+      edges.push_back(std::tuple<T,T>(v1, v2));
+    }
+    if (line) free(line);
+    fclose(f);
+  }
+  assert(edges.size() == M);
+  std::sort(edges.begin(), edges.end());
+  fasttime_t end   = gettime();
+  printf("scale=%2d Edgefactor=%2d K1time: %9.3fs Medges/sec: %7.2f\n", 
+         SCALE, edges_per_vertex, 
+         end-start,
+         1e-6 * M / (end-start));
+}
+template <class T>
+void pagerankpipeline(int SCALE, int edges_per_vertex, int n_files) {
+  kernel0<T>(SCALE, edges_per_vertex, n_files);
+  kernel1<T>(SCALE, edges_per_vertex, n_files);
 }
 
 template void pagerankpipeline<uint32_t>(int SCALE, int edges_per_vertex, int n_files);
