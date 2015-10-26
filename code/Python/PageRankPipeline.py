e37b058ac33ce35d39f499abff6f5b42d5c95380
Vijay Gadepally <vijayg@Vijays-MacBook-Air.local>
2015-10-25 20:26:01 -0400

    removing keyboard code

--- code/Python/PageRankPipeline.py
+++ code/Python/PageRankPipeline.py
@@ -7,24 +7,6 @@ import time
 from numpy import linalg as la
 
 
-# import code
-# import sys
-# def keyboard(banner=None):
-#     ''' Function that mimics the matlab keyboard command '''
-#     # use exception trick to pick up the current frame
-#     try:
-#         raise None
-#     except:
-#         frame = sys.exc_info()[2].tb_frame.f_back
-#     print "# Use quit() to exit :) Happy debugging!"
-#     # evaluate commands in current namespace
-#     namespace = frame.f_globals.copy()
-#     namespace.update(frame.f_locals)
-#     try:
-#         code.interact(banner=banner, local=namespace)
-#     except SystemExit:
-#         return
-
 ###################################################
 ###################################################
 def KronGraph500NoPerm (SCALE, EdgesPerVertex):
